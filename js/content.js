cbed5e5a24faa7778a665c56f6c5b7bf7bbb4e7a
Anthony Cui Desktop <ninjaminer18@gmail.com>
2020-03-18 11:22:56 -0400

    Articles are now matched btwn sites, titles/desc aren't

--- js/content.js
+++ js/content.js
@@ -5,119 +5,181 @@ if (confirm(`I AM HEREEEEE at ${WEBPAGEURL}`)) {
     switch (WEBPAGEURL) {
         case "www.foxnews.com":
             alert("hello?");
+            chrome.storage.local.get("www.vox.com", (data) => {
+                const articles = data["www.vox.com"];
 
-            document.querySelectorAll(".content a").forEach((e) => {
-                if (e.children.length === 0) e.textContent = "oh no im gone :(";
+                var i = 0;
+                document.querySelectorAll(".content a").forEach((e) => {
+                    if (e.children.length === 0) {
+                        e.textContent = articles[i];
+                        i = (i + 1) % articles.length;
+                    }
+                });
             });
             break;
         case "www.vox.com":
             alert("hello?");
+            chrome.storage.local.get("www.foxnews.com", (data) => {
+                const articles = data["www.foxnews.com"];
 
-            document.querySelectorAll(".c-entry-box--compact__body").forEach((e) => {
-                e.querySelector(".c-entry-box--compact__title a").textContent = "oh no im gone :(";
-                e.querySelector(".c-entry-box--compact__dek").textContent = "Im a description!";
+                var i = 0;
+                document.querySelectorAll(".c-entry-box--compact__body").forEach((e) => {
+                    e.querySelector(".c-entry-box--compact__title a").textContent = articles[i];
+                    e.querySelector(".c-entry-box--compact__dek").textContent = articles[i];
+                    i = (i + 1) % articles.length;
+                });
             });
             break;
         case "www.msnbc.com":
             alert("hello?");
+            chrome.storage.local.get("www.breitbart.com", (data) => {
+                const articles = data["www.breitbart.com"];
 
-            document.querySelectorAll(".cover-spread-tease__headline span:not(.icon)").forEach((e) => {
-                e.textContent = "oh no im gone :(";
-            });
+                var i = 0;
+                document.querySelectorAll(".cover-spread-tease__headline span:not(.icon)").forEach((e) => {
+                    e.textContent = articles[i];
+                    i = (i + 1) % articles.length;
+                });
 
-            document.querySelectorAll(".twoByOne h3 a").forEach((e) => {
-                e.textContent = "oh no im gone :(";
-            });
+                document.querySelectorAll(".twoByOne h3 a").forEach((e) => {
+                    e.textContent = articles[i];
+                    i = (i + 1) % articles.length;
+                });
 
-            document.querySelectorAll(".cover-spread__headline").forEach((e) => {
-                e.textContent = "Im a big headline!!!";
+                document.querySelectorAll(".cover-spread__headline").forEach((e) => {
+                    e.textContent = articles[i];
+                    i = (i + 1) % articles.length;
+                });
             });
             break;
         case "www.breitbart.com":
             alert("hello?");
+            chrome.storage.local.get("www.msnbc.com", (data) => {
+                const articles = data["www.msnbc.com"];
 
-            document.querySelectorAll("article").forEach((e) => {
-                e.querySelector("h2 a").textContent = "oh no im gone :(";
-                e.querySelector(".excerpt p").textContent = "im a little blurb!";
-            });
-            document.querySelectorAll("#BBTrendUL a").forEach((e) => {
-                e.textContent = "oh no im gone :(";
-            });
-            document.querySelectorAll("#DQSW em").forEach((e) => {
-                e.textContent = "side article :)";
+                var i = 0;
+                document.querySelectorAll("article").forEach((e) => {
+                    e.querySelector("h2 a").textContent = articles[i];
+                    e.querySelector(".excerpt p").textContent = articles[i];
+                    i = (i + 1) % articles.length;
+                });
+                document.querySelectorAll("#BBTrendUL a").forEach((e) => {
+                    e.textContent = articles[i];
+                    i = (i + 1) % articles.length;
+                });
+                document.querySelectorAll("#DQSW em").forEach((e) => {
+                    e.textContent = articles[i];
+                    i = (i + 1) % articles.length;
+                });
             });
             break;
         case "www.cnn.com":
             alert("hello?");
+            chrome.storage.local.get("www.wsj.com", (data) => {
+                const articles = data["www.wsj.com"];
 
-            document.querySelectorAll(".cd__headline-text").forEach((e) => {
-                e.textContent = "oh no im gone :(";
+                var i = 0;
+                document.querySelectorAll(".cd__headline-text").forEach((e) => {
+                    e.textContent = articles[i];
+                    i = (i + 1) % articles.length;
+                });
             });
             break;
         case "www.wsj.com": //TODO DOESNT WORK
             alert("hello?");
+            chrome.storage.local.get("www.cnn.com", (data) => {
+                const articles = data["www.cnn.com"];
 
-            document.querySelectorAll("article").forEach((e) => {
-                const a = e.querySelector("h3 a");
-                if (a) a.textContent = "oh no im gone :(";
-                const b = e.querySelector("p");
-                if (b) b.textContent = "Im a description!";
+                var i = 0;
+                document.querySelectorAll("article").forEach((e) => {
+                    const a = e.querySelector("h3 a");
+                    if (a) a.textContent = articles[i];
+                    const b = e.querySelector("p");
+                    if (b) b.textContent = articles[i];
+                    i = (i + 1) % articles.length;
+                });
             });
             break;
         case "www.nytimes.com":
             alert("hello?");
+            chrome.storage.local.get("www.infowars.com", (data) => {
 
+            });
             //TODO idk how 2 do plz help
             break;
         case "www.infowars.com":
             alert("hello?");
+            chrome.storage.local.get("www.nytimes.com", (data) => {
+                const articles = data["www.nytimes.com"];
 
-            document.querySelectorAll(".title a").forEach((e) => {
-                e.textContent = "oh no im gone :(";
-            });
-            document.querySelectorAll(".title-ddd").forEach((e) => {
-                e.textContent = "oh no im gone :(";
-            });
-            document.querySelectorAll(".stansberry_item a").forEach((e) => {
-                e.textContent = "wtf is a stansberry";
-            });
-            document.querySelectorAll(".article-content").forEach((e) => {
-                e.querySelector("h3 a").textContent = "Hi im a title!";
-                e.querySelector("h4").textContent = "im a baby summary";
+                var i = 0;
+                document.querySelectorAll(".title a").forEach((e) => {
+                    e.textContent = articles[i];
+                    i = (i + 1) % articles.length;
+                });
+                document.querySelectorAll(".title-ddd").forEach((e) => {
+                    e.textContent = articles[i];
+                    i = (i + 1) % articles.length;
+                });
+                document.querySelectorAll(".stansberry_item a").forEach((e) => {
+                    e.textContent = articles[i];
+                    i = (i + 1) % articles.length;
+                });
+                document.querySelectorAll(".article-content").forEach((e) => {
+                    e.querySelector("h3 a").textContent = articles[i];
+                    e.querySelector("h4").textContent = articles[i];
+                    i = (i + 1) % articles.length;
+                });
             });
             break;
         case "www.theatlantic.com":
             alert("hello?");
+            chrome.storage.local.get("www.theonion.com", (data) => {
+                const articles = data["www.theonion.com"];
 
-            document.querySelectorAll(".c-cover-story__content").forEach((e) => {
-                e.querySelector("h1 a").textContent = "BIG TITLE";
-                e.querySelector("p").textContent = "im also big!";
-            });
+                var i = 0;
 
-            document.querySelectorAll("article").forEach((e) => {
-                const a = e.querySelector("h2 a");
-                if (a) a.textContent = "baby title";
+                document.querySelectorAll(".c-cover-story__content").forEach((e) => {
+                    e.querySelector("h1 a").textContent = articles[i];
+                    e.querySelector("p").textContent = articles[i];
+                    i = (i + 1) % articles.length;
+                });
 
-                const b = e.querySelector("p");
-                if (b) b.textContent = "sorta big";
+                document.querySelectorAll("article").forEach((e) => {
+                    const a = e.querySelector("h2 a");
+                    if (a) a.textContent = articles[i];
 
-                const c = e.querySelector("h3 a");
-                if (c) c.textContent = "hallo";
-            });
+                    const b = e.querySelector("p");
+                    if (b) b.textContent = articles[i];
 
-            document.querySelectorAll(".c-popular__link").forEach((e) => {
-                e.textContent = "oh no im gone :(";
+                    const c = e.querySelector("h3 a");
+                    if (c) c.textContent = articles[i];
+
+                    i = (i + 1) % articles.length;
+                });
+
+                document.querySelectorAll(".c-popular__link").forEach((e) => {
+                    e.textContent = articles[i];
+                    i = (i + 1) % articles.length;
+                });
             });
             break;
         case "www.theonion.com":
             alert("hello?");
+            chrome.storage.local.get("www.theatlantic.com", (data) => {
+                const articles = data["www.theatlantic.com"];
 
-            document.querySelectorAll("article").forEach((e) => {
-                const a = e.querySelector("h4");
-                if (a) a.textContent = "BIG TITLE";
+                var i = 0;
+                document.querySelectorAll("article").forEach((e) => {
+                    console.log(`Article ${i}: ${articles[i]}`);
+                    const a = e.querySelector("h4");
+                    if (a) a.textContent = articles[i];
 
-                const b = e.querySelector("p");
-                if (b) b.textContent = "im also big!";
+                    const b = e.querySelector("p");
+                    if (b) b.textContent = articles[i];
+
+                    i = (i + 1) % articles.length;
+                });
             });
             break;
     }
